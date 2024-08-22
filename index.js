92430b716ee40014b747e63fac66da33c51f98be
Frhan21 <2010441005_m@student.unand.ac.id>
2024-08-22 13:28:09 +0700

    feat: add cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const routes = require('./routes.js');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const app = express(); 
 app.use(bodyParser.json())
 app.use(express.json()); 
 // app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.use(cors())
 app.use('/api', routes); 
 
 const PORT = 3000; 
