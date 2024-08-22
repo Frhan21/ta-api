41277c9386d38aefd1907b336affc2c4e1fd6d29
Frhan21 <2010441005_m@student.unand.ac.id>
2024-08-22 13:05:29 +0700

    fix router

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json())
 app.use(express.json()); 
 // app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.use('/', routes); 
+app.use('/api', routes); 
 
 const PORT = 3000; 
 app.listen(PORT, () => {
